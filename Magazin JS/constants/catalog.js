714ad02a3e3e6a790efde15b9d1077299c9ff100
Johny <43562887+Johny770@users.noreply.github.com>
2020-04-15 21:11:43 +0300

    Add files via upload

---
+++ Magazin JS/constants/catalog.js
@@ -0,0 +1,57 @@
+const CATALOG = [
+    {
+        id: 'el1',
+        name: 'Nike',
+        img: 'img/6.jpg',
+        price: 100,
+    },
+    {
+        id: 'el2',
+        name: 'Nike Air',
+        img: 'img/7.jpg',
+        price: 150,
+    },
+    {
+        id: 'el3',
+        name: 'Nike Max',
+        img: 'img/8.jpg',
+        price: 200,
+    },
+    {
+        id: 'el4',
+        name: 'Nike',
+        img: 'img/9.jpg',
+        price: 170,
+    },
+    {
+        id: 'el5',
+        name: 'Nike',
+        img: 'img/10.jpg',
+        price: 120,
+    },
+    {
+        id: 'el6',
+        name: 'Nike',
+        img: 'img/19.jpg',
+        price: 180,
+    },
+    {
+        id: 'el7',
+        name: 'Nike',
+        img: 'img/16.jpg',
+        price: 220,
+    },
+    {
+        id: 'el8',
+        name: 'Nike',
+        img: 'img/17.jpg',
+        price: 130,
+    },
+    {
+        id: 'el9',
+        name: 'Nike',
+        img: 'img/18.jpg',
+        price: 170,
+    },
+
+];
